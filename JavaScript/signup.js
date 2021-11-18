04307aa336029ec5eda6a5cfaa4befe395c303fe
IamGopal <iamgopal.dev@gmail.com>
2021-11-18 22:37:27 +0530

    basic structure,hompage and some pages are added

---
+++ JavaScript/signup.js
@@ -0,0 +1,10 @@
+function show() {
+  var p = document.getElementById("pwd");
+  p.setAttribute("type", "text");
+}
+
+function hide() {
+  var p = document.getElementById("pwd");
+  p.setAttribute("type", "password");
+}
+
